5abcb435d8b73d6d7ba93c495c95ac478d3636a5
dajjal <turanarican1985@gmail.com>
2024-02-01 18:46:07 +0300

    finished

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 const inputForm = document.querySelector("form");
-const billInput = document.querySelector("#amount");
-const selectedTipOption = document.querySelector('input[name="tip"]:checked');
-const customTip = document.querySelector("#custom");
+const billInput = document.querySelector("#bill-amount");
+const customTipInput = document.querySelector("#custom");
 
-const getTip = function () {
+const labelComputedTipAmount = document.querySelector("#computed-tip-amount");
+const labelComputedTotalAmount = document.querySelector(
+  "#computed-total-amount"
+);
+const numberOfPeopleInput = document.querySelector("#people");
+
+const resetButton = document.querySelector("#reset");
+
+const getTipPercent = function () {
   const selectedTipOption = document.querySelector('input[name="tip"]:checked');
   if (selectedTipOption.id === "percent-custom") {
-    return customTip.value;
+    return Number(customTipInput.value) <= 100 && Number(customTipInput.value);
   }
   return selectedTipOption.value;
 };
 
-inputForm.addEventListener("input", () => {
-  console.log(getTip());
+const renderOutput = function () {
+  const totalTipAmount = (Number(billInput.value) * getTipPercent()) / 100;
+  const tipPerPerson = (
+    totalTipAmount / (Number(numberOfPeopleInput.value) || 1)
+  ).toFixed(2);
+  const totalAmountPerPerson = (
+    (Number(billInput.value) + totalTipAmount) /
+    (Number(numberOfPeopleInput.value) || 1)
+  ).toFixed(2);
+  labelComputedTipAmount.textContent = tipPerPerson;
+  labelComputedTotalAmount.textContent = totalAmountPerPerson;
+};
+
+inputForm.addEventListener("input", renderOutput);
+
+resetButton.addEventListener("click", () => {
+  labelComputedTipAmount.textContent = (0).toFixed(2);
+  labelComputedTotalAmount.textContent = (0).toFixed(2);
 });
